acae0af6080b37acb2b63a840402b81a5fec447b
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2025-07-28 02:25:06 +0300

    Entitys database connection Api Layer created

---
+++ CarBook.Domain/Entities/CarFeatures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class CarFeatures
+    {
+        public int CarFeaturesId { get; set; }
+
+        public int CarId { get; set; }
+        public int FeaturesId { get; set; }
+
+        public Car Car { get; set; }
+        public Features Features { get; set; }
+
+        public bool Available { get; set; }
+    }
+}
---
+++ CarBook.Domain/Entities/FooterAdress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public   class FooterAdress
+    {
+        public int FooterAdressId { get; set; }
+        public string Description    { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string MapUrl { get; set; }
+    }
+}
